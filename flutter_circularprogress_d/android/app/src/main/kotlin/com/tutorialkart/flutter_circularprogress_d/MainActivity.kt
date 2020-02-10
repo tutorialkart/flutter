ba8811f0899b3bc2fd2907827ef5a0e14e3bf1ca
tutorialkart <tutorialskart@gmail.com>
2020-02-10 20:02:36 +0530

    Add files via upload

---
+++ flutter_circularprogress_d/android/app/src/main/kotlin/com/tutorialkart/flutter_circularprogress_d/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tutorialkart.flutter_circularprogress_d
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
